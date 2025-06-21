13e11417c6e8958f730cf7568d74ad58c4fb7cfd
Dmitry Gorbunov <dimarik70rus@gmail.com>
2025-06-21 19:36:53 +0700

    connection sonarqube

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,16 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
+    id("org.sonarqube") version "6.2.0.5505"
     application
 }
-
+sonar {
+    properties {
+        property("sonar.projectKey", "Dimon7091_java-project-61")
+        property("sonar.organization", "dmitry-gorbunov-linter")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
+}
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
 
