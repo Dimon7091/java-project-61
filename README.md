12c60b1c85ca0667831cd4533cc616c806d92d84
Dmitry Gorbunov <dimarik70rus@gmail.com>
2025-06-23 00:54:19 +0700

    add Even game

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/Dimon7091/java-project-61/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/Dimon7091/java-project-61/actions)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=Dimon7091_java-project-61&metric=alert_status)](https://sonarcloud.io/summary/new_code?id=Dimon7091_java-project-61)
+### Игра "Проверка на чётность" (Суть игры в следующем: пользователю показывается случайное число. И ему нужно ответить yes, если число чётное, или no — если нечётное.)
+![EvenDemo](https://github.com/Dimon7091/java-project-61/blob/main/assets/EvenDemo.gif)
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,26 @@
 package hexlet.code;
 
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-        Cli.greeting();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                Please enter the game number and press Enter.
+                1 - Greet
+                2 - Even
+                0 - Exit""");
+        String choice = scanner.next();
+        System.out.println("Your choice: " + choice);
+        if (choice.equals("1")) {
+            Cli.greeting();
+        } else if (choice.equals("2")) {
+            Cli.greeting();
+            Even.game();
+        } else {
+            System.out.println("Incorrect number!");
+
+        }
     }
 }
